72ead6d64544282baff0912fc479f6333287d097
JKL <labbetrv@gmail.com>
2024-03-17 17:47:28 +0100

    Constructing beginning of file

--- DanCutter.py
+++ DanCutter.py
@@ -1,13 +1,24 @@
+import re
+import os
+
+def open_in_notepad(file_path):
+    os.system('start notepad.exe ' + file_path)
+
+def extractDiffName(input_text):
+    pattern = re.search(r"^.*-.*\s\(.*\)\s\[(.*)\]", input_text)
+    return pattern.groups()
+
 def breakFinder(f,start):
+    breaks = []
     for l_no,line in enumerate(f,start):
         previous = extractTime(line)
         break
     for l_no, line in enumerate(f,start=start+1):
         next = extractTime(line)
         if gapAnalyser(previous,next):
-            test = int(input("test"))
-            print("break")
+            breaks.append(l_no)
         previous = next
+    return breaks
 
 def extractTime(line):
     noteData = line.split(',')
@@ -15,12 +26,53 @@ def extractTime(line):
 
 def gapAnalyser(note1,note2):
     return note2-note1 >= 3000
+
+def createFile(start,end,directory,diffName,ogFile):
+    with open(f"{directory}{diffName}.osu", "w") as file:
+        dataLine = extractGeneralEditorData(ogFile)
+        file.write(dataLine[0])
+        file.write(f"Version:{diffName}\n")
+        dataLine = extractSourceTagsBeatmapID(ogFile,dataLine[1]+1)
+        file.write(dataLine[0])
+        file.write("BeatmapSetID:-1\n")
+        file.write("quoi")
+        open_in_notepad(f"{directory}{diffName}.osu")
     
-f = open("C:\\Users\\labbe\\AppData\\Local\\osu!\\Songs\\1116467 Various Artists - 4K LN Dan Courses v2 - FINAL -\\Various Artists - 4K LN Dan Courses v2 - FINAL - (_underjoy) [15th Dan - Yume (Marathon)].osu", "r")
+def extractSourceTagsBeatmapID(f,start):
+    result = ""
+    endLine = 0
+    with open(f,"r") as file:
+        for l_no,i in enumerate(file):
+            if l_no >= start-1:
+                if 'BeatmapSetID:' in i:
+                    endLine = l_no+1
+                    break
+                result = result + i
+    return result,endLine
+    
+def extractGeneralEditorData(f):
+    result = ""
+    endLine = 0
+    with open(f,"r") as file:
+        for l_no,i in enumerate(file):
+            if 'Version:' in i:
+                endLine = l_no+1
+                break
+            result = result + i
+    return result,endLine
+     
+file = "C:\\Users\\labbe\\AppData\\Local\\osu!\\Songs\\1116467 Various Artists - 4K LN Dan Courses v2 - FINAL -\\Various Artists - 4K LN Dan Courses v2 - FINAL - (_underjoy) [15th Dan - Yume (Marathon)].osu"
+dir = ""
+path = file.split("\\")
+for i in range(0,len(path)-1):
+    dir = dir + path[i] + "\\"
+diffName = path[-1][0:-4]
+f = open(file, "r")
 no = 0
 for l_no, line in enumerate(f):
     if '[HitObjects]' in line:
         no = l_no + 1
         break
-breakFinder(f,no)
+breaks = breakFinder(f,no)
+createFile(no,breaks[0],dir,extractDiffName(diffName)[0] + " - 1st Map",file)
 f.close()
