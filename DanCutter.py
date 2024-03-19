958cf45be13a4b8a2773bdafcf11b85bd0c17f7b
JKL <labbetrv@gmail.com>
2024-03-19 23:54:02 +0100

    All functions implemented

--- DanCutter.py
+++ DanCutter.py
@@ -1,13 +1,7 @@
 import re
 import os
-import math
 
-def open_in_notepad(file_path):
-    os.system('start notepad.exe ' + file_path)
-
-def extractDiffName(input_text):
-    pattern = re.search(r"^.*-.*\s\(.*\)\s\[(.*)\]", input_text)
-    return pattern.groups()
+# Processing functions
 
 def breakFinder(f,start):
     breaksNo = []
@@ -23,29 +17,8 @@ def breakFinder(f,start):
         previous = next
     return breaksNo,breaksTPoints
 
-def extractTime(line):
-    noteData = line.split(',')
-    return int(noteData[2])
-
 def gapAnalyser(note1,note2):
     return note2-note1 >= 3000
-
-def createFile(startingLine,endingLine,startingTPoint,endingTPoint,directory,diffName,ogFile):
-    with open(f"{directory}{diffName}.osu", "w") as file:
-        dataLine = extractData(ogFile,0,'Version:')
-        file.write(dataLine[0])
-        file.write(f"Version:{diffName}\n")
-        dataLine = extractData(ogFile,dataLine[1]+1,'BeatmapSetID:')
-        file.write(dataLine[0])
-        file.write("BeatmapSetID:-1\n")
-        dataLine = extractData(ogFile,dataLine[1]+1,'[TimingPoints]')
-        file.write(dataLine[0])
-        dataLine = extractData(ogFile,dataLine[1]+1,'[HitObjects]')
-        print(processTimingPoints(dataLine[0],startingTPoint,endingTPoint)) # Process Timing Points
-        return
-        file.write('[HitObjects]\n')
-        file.write(extractNoteData(ogFile,startingLine,endingLine))
-        open_in_notepad(f"{directory}{diffName}.osu")
 
 def processTimingPoints(tPoints,start,end):
     tPointsTab = tPoints.split()
@@ -54,33 +27,69 @@ def processTimingPoints(tPoints,start,end):
         lineValue = tPointsTab[i].split(',')
         if int(lineValue[0]) >= start and int(lineValue[0]) <= end:
             tPointsTabFiltered.append(tPointsTab[i])
-    print(findGlobalBpm(tPointsTabFiltered))
-    # for i in range(0,len(tPointsTabFiltered)-1):
-    #     tPoint1 = tPointsTabFiltered[i].split(',')
-    #     tPoint2 = tPointsTabFiltered[i+1].split(',')
-    #     if int(tPoint1[6]) == 1 and int(tPoint2[6]) == 0:
-    #         tPoint3 = tPointsTabFiltered[i+2].split(',')
-    #         res = float(tPoint3[1])/float(tPoint1[1])
-    #         print(res)
-    return
-    for i in range(0,len(tPointsTab)-1):
-        pass
-    return
+    mapBeatLength = float(tPointsTabFiltered[0].split(',')[1])
+    for i in range(0,len(tPointsTabFiltered)-1):
+        tPoint1 = tPointsTabFiltered[i].split(',')
+        tPoint2 = tPointsTabFiltered[i+1].split(',')
+        if int(tPoint1[6]) == 1 and int(tPoint2[6]) == 0:
+            tPoint2[1] = sliderVelocityMultiplierConvertor(float(tPoint1[1])/mapBeatLength)
+            tPointsTabFiltered[i+1] = listToString(tPoint2)
+    tPointsString = ""
+    for i in range(0,len(tPointsTabFiltered)):
+        tPointsString = tPointsString + tPointsTabFiltered[i] + "\n"
+    return tPointsString
 
-def findGlobalBpm(tPointsTab):
-    for i in range(0,len(tPointsTab)-1):
-        print(tPointsTab[i].split(',')[6] == 1 and tPointsTab[i+1].split(',')[6] == 0)
-        if int(tPointsTab[i].split(',')[6]) == 1 and int(tPointsTab[i+1].split(',')[6]) == 0:
-            return calculateGlobalBpm(float(tPointsTab[i].split(',')[1]),float(tPointsTab[i+1].split(',')[1]))
+def listToString(list):
+    res = ""
+    for i in list:
+        res = res + str(i) + ','
+    return res[0:-1]
 
 def calculateGlobalBpm(bpm,sliderVelocity):
-    return bpmToBeatPerSecond(bpm)*negativeSliderVelocityMultiplierToMultiplier(sliderVelocity)
+    return bpmToBeatPerSecond(bpm)*sliderVelocityMultiplierConvertor(sliderVelocity)
 
 def bpmToBeatPerSecond(bpm):
     return 1 / bpm * 1000 * 60
 
-def negativeSliderVelocityMultiplierToMultiplier(number):
+def sliderVelocityMultiplierConvertor(number):
     return 1/-number*100
+    
+def countLinesInFile(file):
+    with open(f"{file}", 'r') as fp:
+        for count, line in enumerate(fp):
+            pass
+    return (count+1)
+
+# Variables extraction functions
+
+def getLastLineOfFileTime(file):
+    with open(f'{file}', 'rb') as f:
+        try:
+            f.seek(-2, os.SEEK_END)
+            while f.read(1) != b'\n':
+                f.seek(-2, os.SEEK_CUR)
+        except OSError:
+            f.seek(0)
+        last_line = f.readline().decode().split(',')
+        return int(last_line[2])
+
+def extractDiffName(input_text):
+    pattern = re.search(r"^.*-.*\s\(.*\)\s\[(.*)\]", input_text)
+    return pattern.groups()[0]
+
+def extractMapTitle(input_text):
+    pattern = re.search(r"^(.*)\s\[.*\]$",input_text)
+    return pattern.groups()[0]
+
+def extractTime(line):
+    noteData = line.split(',')
+    return int(noteData[2])
+
+def getMapBpm(tPointsTab):
+    return round(bpmToBeatPerSecond(float(tPointsTab[0].split(',')[1])),3)
+
+def getGlobalBpm(tPointsTab):
+    return round(calculateGlobalBpm(float(tPointsTab[0].split(',')[1]),float(tPointsTab[1].split(',')[1])),3)
 
 def extractNoteData(f,start,end):
     result = ""
@@ -103,22 +112,56 @@ def extractData(f,start,stopPoint):
                     break
                 result = result + i
     return result,endLine
-     
+
+# File manipulation functions
+
+def buildMapName(oldMapName,mapNumber):
+    return f"{extractMapTitle(oldMapName)} [{extractDiffName(oldMapName)} - Map {mapNumber}"
+
+def createFile(startingLine,endingLine,startingTPoint,endingTPoint,directory,mapName,ogFile,mapNumber):
+    with open(f"{directory}{buildMapName(mapName,mapNumber)}].osu", "w") as file:
+        dataLine = extractData(ogFile,0,'PreviewTime:')
+        file.write(dataLine[0])
+        file.write(f"PreviewTime: {round((int(startingTPoint)+int(endingTPoint))/2)}\n")
+        dataLine = extractData(ogFile,dataLine[1]+1,'Version:')
+        file.write(dataLine[0])
+        file.write(f"Version: {extractDiffName(mapName)} - Map {mapNumber}\n")
+        dataLine = extractData(ogFile,dataLine[1]+1,'BeatmapSetID:')
+        file.write(dataLine[0])
+        file.write("BeatmapSetID: -1\n")
+        dataLine = extractData(ogFile,dataLine[1]+1,'[TimingPoints]')
+        file.write(dataLine[0])
+        dataLine = extractData(ogFile,dataLine[1]+1,'[HitObjects]')
+        file.write('[TimingPoints]\n')
+        file.write(processTimingPoints(dataLine[0],startingTPoint,endingTPoint))
+        file.write('\n[HitObjects]\n')
+        file.write(extractNoteData(ogFile,startingLine,endingLine))
+        # open_in_notepad(f"{directory}{buildMapName(mapName,mapNumber)}].osu")
+
+# Debug functions
+
+def open_in_notepad(file_path):
+    os.system('start notepad.exe ' + file_path)
+
+# Main program
+    
 file = "C:\\Users\\labbe\\AppData\\Local\\osu!\\Songs\\1116467 Various Artists - 4K LN Dan Courses v2 - FINAL -\\Various Artists - 4K LN Dan Courses v2 - FINAL - (_underjoy) [15th Dan - Yume (Marathon)].osu"
-file = "C:\\Users\\labbe\\AppData\\Local\\osu!\\Songs\\1919657 Various Artists - Dan ~ PRE-DELTA ~ Courses v2\\Various Artists - Dan ~ PRE-DELTA ~ Courses v2 (Sakisagee) [Pre-Delta v5].osu"
+file = "C:\\Users\\labbe\\AppData\\Local\\osu!\\Songs\\Various_Artists_-_Chordjack_Joker_Dan\\Various Artists - Chordjack Joker Dan ([Crz]Rachel&Ice V) [Joker - CHI E_uOCy].osu"
 dir = ""
 path = file.split("\\")
 for i in range(0,len(path)-1):
     dir = dir + path[i] + "\\"
-diffName = path[-1][0:-4]
-f = open(file, "r")
-no = 0
-for l_no, line in enumerate(f):
-    if '[HitObjects]' in line:
-        no = l_no + 1
-        break
-breaks = breakFinder(f,no)
-breaksLines = breaks[0]
-breaksTPoints = breaks[1]
-createFile(no,breaksLines[0],0,breaksTPoints[0],dir,extractDiffName(diffName)[0] + " - 1st Map",file)
-f.close()
+mapName = path[-1][0:-4]
+with open(file, "r") as f:
+    no = 0
+    for l_no, line in enumerate(f):
+        if '[HitObjects]' in line:
+            no = l_no + 1
+            break
+    breaks = breakFinder(f,no)
+    breaksLines = breaks[0]
+    breaksTPoints = breaks[1]
+    createFile(no,breaksLines[0],0,breaksTPoints[0],dir,mapName,file,1)
+    for i in range(1,len(breaksLines)):
+        createFile(breaksLines[i-1],breaksLines[i],breaksTPoints[i-1],breaksTPoints[i],dir,mapName,file,i+1)
+    createFile(breaksLines[-1],countLinesInFile(file),breaksTPoints[-1],getLastLineOfFileTime(file),dir,mapName,file,len(breaksLines)+1)
--- test.py
+++ test.py
@@ -3,3 +3,8 @@ var2 = -100
 var3 = 1/-var2*100
 print(var3)
 print(1 / var1 * 1000 * 60 * var3)
+var4 = 237.154150197629
+var5 = 213.827512473271
+print(var5/var4)
+var6 = 0.79
+print(1/-var6*100)
